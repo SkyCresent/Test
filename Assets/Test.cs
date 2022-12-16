190cb13babda69a2fbbe7a9667e8aecc35539f1d
asqdg7724 <81556356+SkyCresent@users.noreply.github.com>
2022-12-16 12:17:48 +0900

    인트를 더블로

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,8 +7,8 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int test;
-        test = 1004123;
+        double test;
+        test = 1004;
     }
 
     // Update is called once per frame
